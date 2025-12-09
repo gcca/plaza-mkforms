2974912d4123e1bdced8a53f687fb1e7f9173b62
gcca <git@gcca.dev>
2025-12-08 21:40:49 -0500

    20251208214049

--- plaza_mkforms/views.py
+++ plaza_mkforms/views.py
@@ -1,4 +1,5 @@
 import io
+import lzma
 
 import django.forms
 import django.http
@@ -63,7 +64,8 @@ class SettingForm(django.forms.ModelForm):
     def save(self, commit=True):
         instance = super().save(commit=False)
         if self.cleaned_data.get("logo"):
-            instance.logo = self.cleaned_data["logo"].read()
+            data = self.cleaned_data["logo"].read()
+            instance.logo = lzma.compress(data, preset=lzma.PRESET_EXTREME)
         if commit:
             instance.save()
         return instance
