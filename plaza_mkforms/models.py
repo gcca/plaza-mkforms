9e4d24b2bdcb7e4333fa05e295983279d3da0bd5
gcca <git@gcca.dev>
2025-12-08 21:25:22 -0500

    20251208212522

--- plaza_mkforms/models.py
+++ plaza_mkforms/models.py
@@ -5,3 +5,15 @@ class DocumentAA(django.db.models.Model):
     name = django.db.models.CharField(max_length=100)
     amount = django.db.models.IntegerField()
     quantity = django.db.models.IntegerField()
+    created_at = django.db.models.DateTimeField(auto_now_add=True)
+
+
+class Setting(django.db.models.Model):
+    name = django.db.models.CharField(max_length=100)
+    logo = django.db.models.BinaryField(null=True)
+
+    class Meta:
+        indexes = [django.db.models.Index(fields=["name"])]
+
+    def __str__(self) -> str:
+        return f"{self.name}"
