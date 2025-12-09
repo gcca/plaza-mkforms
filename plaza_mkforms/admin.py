35b476e1315eea6188c9fccace9dddfa13ab6c6a
gcca <git@gcca.dev>
2025-12-09 06:52:06 -0500

    20251209065206

--- plaza_mkforms/admin.py
+++ plaza_mkforms/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+import django.contrib.admin
+
+import plaza_mkforms.models
+
+
+@django.contrib.admin.register(plaza_mkforms.models.DocumentAA)
+class DocumentAAAdmin(django.contrib.admin.ModelAdmin):
+    list_display = ["name", "amount", "quantity", "created_at"]
+    search_fields = ["name"]
+    list_filter = ["created_at"]
+    ordering = ["-created_at"]
+
+
+@django.contrib.admin.register(plaza_mkforms.models.Setting)
+class SettingAdmin(django.contrib.admin.ModelAdmin):
+    list_display = ["name", "logo"]
+    search_fields = ["name"]
